e1110d2398a058ba39442d60a75373b0d8a53e54
Mariam Jaludi <mariam.jaludi@gmail.com>
2018-11-16 17:38:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,15 +4,31 @@ var temp = currentLine.length +1;
 }
 
 function nowServing(katzDeliLine){
-  if(katzDeliLine.length === 0)
-  {
+  if(katzDeliLine.length === 0){
     return "There is nobody waiting to be served!"
-  }
-  else{
+    }
+  else {
     var temp = katzDeliLine[0];
     katzDeliLine.shift();
     return temp;
-    
+    }
   }
+
+  function currentLine(currentLine){
+    if (currentLine.length != 0){
+      var sentence = "The line is currently: ";
+      for (var i=0; i < currentLine.length; i++){
+        var temp = i + 1;
+        if (currentLine.length === 1 || i === currentLine.length -1){
+          sentence = sentence + `${temp}. ${currentLine[i]}.`
+        }
+        else{
+          sentence = sentence + `${temp}. ${currentLine[i]},`;
+        }  
+      }
+      return sentence;
+    }
+    else return "The line is currently empty."
+  
   }
-}
+
