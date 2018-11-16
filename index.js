afc3a9d767ec5be16507271d74f6cc8cad193622
Mariam Jaludi <mariam.jaludi@gmail.com>
2018-11-16 16:42:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(currentLine, name){
 var temp = currentLine + 1;
-  return `Welcome, ${name}. You are ${temp} in line.`
+  return `Welcome, ${name}. You are number ${temp} in line.`
 }
