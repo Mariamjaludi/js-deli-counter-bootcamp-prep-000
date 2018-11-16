12001482b1a1eb1a4182918dc4687d4b80115114
Mariam Jaludi <mariam.jaludi@gmail.com>
2018-11-16 16:48:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 function takeANumber(currentLine, name){
 var temp = currentLine.length +1;
-  return `Welcome, ${name}. You are number ${temp} in line.`
+  return `Welcome, ${name}. You are number ${temp} in line.`;
+}
+
+function nowServing(katzDeliLine){
+  if(katzDeliLine.length === 0)
+  {
+    return "There is nobody waiting to be served!"
+  }
+  else{
+    var temp = katzDeliLine[0];
+    katzDeliLine.shift();
+    return temp;
+    
+  }
+  }
 }
